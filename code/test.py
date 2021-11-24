a578997800efe61e8040bbfb582937de5df9c5be
Han <han_cai1@brown.edu>
2021-11-23 22:45:30 -0500

    update masks

--- code/test.py
+++ code/test.py
@@ -1,3 +1,10 @@
 import numpy as np
+import skimage.io as io
+import matplotlib.pyplot as plt
+
 
 print(np.array([1,2,3]))
+
+img = io.imread('/Users/mike/Desktop/Computer Vision/cs1430_projects/face_changing/masks/1.png')
+plt.imshow(img)
+plt.show()
